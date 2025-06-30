d7b7603efe2275611b6035f995ed32e99405b80d
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2025-06-30 16:55:34 +0800

    feat: enhance object tracking with ID assignment
    This commit improves the object tracking system by introducing ID
    assignment and matching logic based on distance. Added parameters
    for marker scaling, frame ratio, missed object threshold, and max
    distance threshold. Updated visualization to include object IDs
    and enhanced marker publishing logic. Removed unused color logic
    and optimized detection callback flow.

--- ros1_ws/src/detr_inference/src/detr_inference_distance.py
+++ ros1_ws/src/detr_inference/src/detr_inference_distance.py
@@ -26,7 +26,7 @@ rospack = rospkg.RosPack()
 
 class DetrInferenceSearchingDistanceNode:
     def __init__(self):
-        rospy.init_node('detr_inference_searching_distance', anonymous=True)
+        rospy.init_node('detr_inference_distance', anonymous=True)
         
         self.rospack = rospkg.RosPack()
         self.bridge = CvBridge()
@@ -40,7 +40,7 @@ class DetrInferenceSearchingDistanceNode:
         
         # Load class names and colors
         self.class_list = self.load_classes()
-        self.class_colors = {class_name: self.colors[i % len(self.colors)] for i, class_name in enumerate(self.class_list)}
+        # self.class_colors = {class_name: self.colors[i % len(self.colors)] for i, class_name in enumerate(self.class_list)}
         
         # Publishers
         self.init_publishers()
@@ -57,6 +57,29 @@ class DetrInferenceSearchingDistanceNode:
         self.cam_ext_rotation = [-60, 0, 60]
         self.hfov = 60.0
 
+        self.previous_bboxes = []
+        self.next_id = 0
+
+        self.fixed_id_colors = {
+            0: self.name_to_bgr("red"),
+            1: self.name_to_bgr("orange"),
+            2: self.name_to_bgr("yellow"),
+            3: self.name_to_bgr("green"),
+        }
+
+        self.previous_objects = {}  # {id: {'center': (x, y), 'missed': int}}
+        self.tracked_ids_in_this_frame = set()
+        self.id_counter = 0
+        
+        self.detection_frame = 1
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        
+
+    def get_fixed_color_for_id(self, obj_id):
+        return self.fixed_id_colors[obj_id % 4]
+
     def load_parameters(self):
         """Load ROS parameters."""
         self.classes_path = rospy.get_param('~classes_path', os.path.join(self.rospack.get_path("detr_inference"), "classes", "KS_Buoy_classes.txt"))
@@ -69,10 +92,18 @@ class DetrInferenceSearchingDistanceNode:
         self.M_L = np.load(rospy.get_param("~h1_path"), None)
         self.M_R = np.load(rospy.get_param("~h2_path"), None)
         crop_str = rospy.get_param("~crop_rect", None)
+        # TF
         self.source_frame = rospy.get_param('~source_frame_id', 'base_link')
         self.target_frame = rospy.get_param('~target_frame_id', 'map')
-        self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.markers_scale = float(rospy.get_param('~markers_scale', 100))
+        # Avoid delaying
+        self.keep_frame_ratio = min(rospy.get_param('~keep_frame_ratio', 0.3), 1.0)
+        self.keep_frame = int(1 / self.keep_frame_ratio)
+        # ID tracking
+        self.max_missed = rospy.get_param('~max_missed', 3)
+        self.max_distance_threshold = rospy.get_param('~max_distance_threshold', 50)
+        self.max_id = rospy.get_param('~max_id', 10)
+        
         self.image_width = None
         if crop_str:
             try:
@@ -87,11 +118,6 @@ class DetrInferenceSearchingDistanceNode:
         else:
             self.crop_rect = None
 
-        # Colors for bounding boxes
-        self.colors = ["cyan", "orange", "red", "yellow", "green", "purple", "blue", "magenta",
-                       "lime", "pink", "teal", "lavender", "brown", "beige", "maroon", "mint",
-                       "olive", "apricot", "navy", "grey", "white", "black"]
-        # self.colors = ["yellow",]
 
     def load_classes(self):
         """Load class names from the specified file."""
@@ -131,41 +157,45 @@ class DetrInferenceSearchingDistanceNode:
         rgb = [int(x * 255) for x in rgb]
         return (rgb[2], rgb[1], rgb[0])  # Convert RGB to BGR
     
-    def draw_detections(self, image, detection_list):
-        for score, class_name, box, distance, angle in detection_list:
-            color_name = self.class_colors.get(class_name, "white")
-            box_color = self.name_to_bgr(color_name)
+    def draw_detections(self, image, matched_results):
+        for obj_id, score, class_name, box, distance, angle in matched_results:
+            box_color = self.get_fixed_color_for_id(obj_id)
             x, y, x2, y2 = [int(i) for i in box]
             cv2.rectangle(image, (x, y), (x2, y2), box_color, 2)
+
+            # ID text
+            text_id = f"id: {obj_id}"
+            text_id_y = y - 60
+            cv2.putText(image, text_id, (x, text_id_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+            cv2.putText(image, text_id, (x, text_id_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
             # Class text
             text_top_class = f"class: {class_name}"
             text_top_class_y = y - 40
-            cv2.putText(image, text_top_class, (x, text_top_class_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+            cv2.putText(image, text_top_class, (x, text_top_class_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             cv2.putText(image, text_top_class, (x, text_top_class_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
             # Confidence text
             text_top_conf = f"conf: {score:.2f}"
             text_top_conf_y = text_top_class_y + 20
-            cv2.putText(image, text_top_conf, (x, text_top_conf_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+            cv2.putText(image, text_top_conf, (x, text_top_conf_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             cv2.putText(image, text_top_conf, (x, text_top_conf_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
             # Distance text
             distance = int(distance) if distance is not None else "N/A"
             text_buttom_distance = f"dist: {distance} m"
             text_buttom_distance_y = y2 + 20 if y2 + 20 < image.shape[0] - 10 else y - 20
-            cv2.putText(image, text_buttom_distance, (x, text_buttom_distance_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+            cv2.putText(image, text_buttom_distance, (x, text_buttom_distance_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             cv2.putText(image, text_buttom_distance, (x, text_buttom_distance_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
             # Angle text
             angle = int(angle) if angle is not None else "N/A"
             text_buttom_angle = f"angle: {angle} deg"
             text_buttom_angle_y = text_buttom_distance_y + 20 if text_buttom_distance_y + 20 < image.shape[0] - 10 else y - 40
-            cv2.putText(image, text_buttom_angle, (x, text_buttom_angle_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+            cv2.putText(image, text_buttom_angle, (x, text_buttom_angle_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             cv2.putText(image, text_buttom_angle, (x, text_buttom_angle_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
         return image
-
 
     def horizon_poly_callback(self, msg):
         cam_names = ['left', 'mid', 'right']
@@ -183,6 +213,11 @@ class DetrInferenceSearchingDistanceNode:
         self.lastest_annotated_image = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="bgr8")
     
     def detection_callback(self, msg):
+        if self.detection_frame >= self.keep_frame:
+            self.detection_frame = 1
+        else:
+            self.detection_frame += 1
+            return
         
         start_time = time.time()
         msg_header = msg.header
@@ -201,27 +236,27 @@ class DetrInferenceSearchingDistanceNode:
             start_time = time.time()
             detections = self.detect_objects(pil_image)
 
-            detection_list = []
-
             if len(detections["scores"]) > 0:
                 scores = detections["scores"].detach().cpu().numpy().tolist()
                 labels = detections["labels"].detach().cpu().numpy().tolist()
                 boxes = detections["boxes"].detach().cpu().numpy().astype(np.int32).tolist()
                 
-                rospy.loginfo(f"scores: {scores}, labels: {labels}, boxes: {boxes}")
-
+                # rospy.loginfo(f"scores: {scores}, labels: {labels}, boxes: {boxes}")
+                
+                current_bboxes = []
                 for score, label_id, box in zip(scores, labels, boxes):
                     x1, y1, x2, y2 = box
-                    center_x = (x1 + x2) / 2
+                    cx = (x1 + x2) / 2
+                    cy = (y1 + y2) / 2
 
-                    if center_x < self.image_width / 3:
+                    if cx < self.image_width / 3:
                         cam_idx = 0
-                    elif center_x < 2 * self.image_width / 3:
+                    elif cx < 2 * self.image_width / 3:
                         cam_idx = 1
                     else:
                         cam_idx = 2
 
-                    mapped_cord = self.pano_to_original((center_x, y2), cam_idx, self.image_width)
+                    mapped_cord = self.pano_to_original((cx, y2), cam_idx, self.image_width)
                     if mapped_cord is None:
                         continue
 
@@ -233,31 +268,73 @@ class DetrInferenceSearchingDistanceNode:
                         continue
 
                     class_name = self.model.config.id2label[label_id]
-                    detection_list.append((score, class_name, box, distance, angle))
+                    current_bboxes.append((cx, cy, (score, class_name, box, distance, angle)))
                 
-                if detection_list:
+                # Perform matching
+                matched_results = self.match_bboxes_by_distance(self.previous_bboxes, current_bboxes)
+                self.previous_bboxes = [(obj_id, self.previous_objects[obj_id]['center']) for obj_id in self.tracked_ids_in_this_frame]
+                
+                if matched_results:
                     self.detected = True
-                    transform = self.tf_buffer.lookup_transform(
-                        self.target_frame,
-                        self.source_frame,
-                        rospy.Time(0),
-                        rospy.Duration(0.5)
-                    )
-                    pose_stamped = PoseStamped()
-                    pose_stamped.header.frame_id = self.target_frame
-                    pose_stamped.header.stamp = rospy.Time.now()
-                    pose_stamped.pose.position.x = float(distance)
-                    pose_stamped.pose.position.y = -float(distance * math.tan(math.radians(angle)))
-                    pose_stamped.pose.orientation.w = 1.0
-                    pose_transformed = tf2_geometry_msgs.do_transform_pose(pose_stamped, transform)
-                    self.publish_detection_markers(detection_list, pose_transformed)
+                    marker_array = MarkerArray()
+
+                    # Clear previous markers
+                    delete_marker = Marker()
+                    delete_marker.action = Marker.DELETEALL
+                    marker_array.markers.append(delete_marker)
+
+                    for detection in matched_results:
+                        obj_id, score, class_name, box, distance, angle = detection
+
+                        pose_stamped = PoseStamped()
+                        pose_stamped.header.frame_id = self.target_frame
+                        pose_stamped.header.stamp = rospy.Time.now()
+                        pose_stamped.pose.position.x = float(distance)
+                        pose_stamped.pose.position.y = -float(distance * math.tan(math.radians(angle)))
+                        pose_stamped.pose.position.z = 0.0
+                        pose_stamped.pose.orientation.w = 1.0
+
+                        try:
+                            transform = self.tf_buffer.lookup_transform(
+                                self.target_frame,
+                                self.source_frame,
+                                rospy.Time(0),
+                                rospy.Duration(0.5)
+                            )
+                            pose_transformed = tf2_geometry_msgs.do_transform_pose(pose_stamped, transform)
+                        except Exception as e:
+                            rospy.logwarn(f"Transform failed for obj_id {obj_id}: {e}")
+                            return
+
+                        marker = Marker()
+                        marker.header.frame_id = self.target_frame
+                        marker.header.stamp = rospy.Time.now()
+                        marker.ns = "detected_object"
+                        marker.id = obj_id
+                        marker.type = Marker.CUBE
+                        marker.action = Marker.ADD
+                        marker.pose = pose_transformed.pose
+
+                        marker.scale.x = self.markers_scale
+                        marker.scale.y = self.markers_scale
+                        marker.scale.z = self.markers_scale
+                        bgr = self.get_fixed_color_for_id(obj_id)
+                        marker.color.b = bgr[0] / 255.0
+                        marker.color.g = bgr[1] / 255.0
+                        marker.color.r = bgr[2] / 255.0
+                        marker.color.a = 0.7
+
+                        marker_array.markers.append(marker)
+
+                    self.pub_marker_array.publish(marker_array)
+
                 
                 rospy.loginfo("Detection processing took: %f seconds", time.time() - start_time)
 
             if self.pub_detection_image_enabled:
                 try:
                     if self.detected:
-                        processed_image = self.draw_detections(self.lastest_annotated_image, detection_list)
+                        processed_image = self.draw_detections(self.lastest_annotated_image, matched_results)
                     else:
                         processed_image = self.lastest_annotated_image
                     self.detected = False
@@ -339,37 +416,59 @@ class DetrInferenceSearchingDistanceNode:
         angle = angle_min + (angle_max - angle_min) * x_ratio
         return angle
 
-    def publish_detection_markers(self, detection_list, pose_transformed):
-        marker_array = MarkerArray()
+    def match_bboxes_by_distance(self, prev_boxes, curr_boxes):
+        """
+        prev_boxes: list of (id, (cx, cy))
+        curr_boxes: list of (cx, cy, (score, class_name, box, distance, angle))
+        return: list of (id, score, class_name, box, distance, angle)
+        """
+        matched_result = []
+        used_prev_ids = set()
 
-        # Delete all previous markers
-        delete_marker = Marker()
-        delete_marker.action = Marker.DELETEALL
-        marker_array.markers.append(delete_marker)
+        self.tracked_ids_in_this_frame = set()
 
-        for idx, detection in enumerate(detection_list):
-            score, class_name, box, distance, angle = detection
-            x1, y1, x2, y2 = box
-            d_y = abs(y2-y1)
+        for curr in curr_boxes:
+            cx, cy, info = curr
+            score, class_name, box, distance, angle = info
+            min_dist = float("inf")
+            matched_id = None
 
-            marker = Marker()
-            marker.header.frame_id = self.target_frame
-            marker.header.stamp = rospy.Time.now()
-            marker.ns = "detected_object"
-            marker.id = idx
-            marker.type = Marker.CUBE
-            marker.action = Marker.ADD
-            marker.pose = pose_transformed.pose
-            marker.scale.x = 100.0
-            marker.scale.y = 100.0
-            marker.scale.z = 100.0
-            marker.color.r = 0.0
-            marker.color.g = 0.0
-            marker.color.b = 1.0
-            marker.color.a = 0.7
-            marker_array.markers.append(marker)
+            for pid, (pcx, pcy) in prev_boxes:
+                if pid in used_prev_ids:
+                    continue
+                dist = math.hypot(cx - pcx, cy - pcy)
+                if dist < min_dist and dist < self.max_distance_threshold:
+                    min_dist = dist
+                    matched_id = pid
 
-        self.pub_marker_array.publish(marker_array)
+            if matched_id is not None:
+                used_prev_ids.add(matched_id)
+            else:
+                matched_id = self.get_next_available_id()
+                if matched_id == -1:
+                    continue
+
+            matched_result.append((matched_id, score, class_name, box, distance, angle))
+            self.previous_objects[matched_id] = {'center': (cx, cy), 'missed': 0}
+            self.tracked_ids_in_this_frame.add(matched_id)
+
+        for obj_id in list(self.previous_objects.keys()):
+            if obj_id not in self.tracked_ids_in_this_frame:
+                self.previous_objects[obj_id]['missed'] += 1
+            # rospy.loginfo(f"obj_id={obj_id}, missed={self.previous_objects[obj_id]['missed']}")
+            if self.previous_objects[obj_id]['missed'] >= self.max_missed:
+                # rospy.loginfo(f"missed count for obj_id={obj_id}: {self.previous_objects[obj_id]['missed']}")
+                del self.previous_objects[obj_id]
+
+        return matched_result
+    
+    def get_next_available_id(self):
+        used_ids = set(self.previous_objects.keys()) | self.tracked_ids_in_this_frame
+        for i in range(self.max_id):
+            if i not in used_ids:
+                return i
+        rospy.logwarn("No available ID found. Consider increasing max_id.")
+        return -1
         
 if __name__ == '__main__':
     try:
